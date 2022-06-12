7f0387d1058c3980ea02d13ab22c822d21f1f2de
mchis1908 <mchis19.08@gmail.com>
2022-06-12 16:33:26 +0700

    Create Order Detail+ Order+ Router DOM

--- uber-eats-dashboard/src/App.js
+++ uber-eats-dashboard/src/App.js
@@ -1,11 +1,35 @@
+import DetailedOrder from './modules/DetailedOrder';
+import Orders from './modules/Orders';
+import {Routes, Route} from 'react-router-dom';
+import { Layout, Image } from "antd";
+
+const { Sider, Content, Footer } = Layout;
 
 
 function App() {
   return (
-    <div className="App">
-     <span>Hello</span>
-    </div>
+    <Layout>
+      <Sider style={{ height: "100vh", backgroundColor: "white" }}>
+          <Image
+            src="https://logos-world.net/wp-content/uploads/2020/11/Uber-Eats-Symbol.jpg"
+            preview={false}
+          />
+        </Sider>
+        <Layout>
+          <Content>
+            <Routes>
+              <Route path="" element={<Orders/>}/>
+              <Route path="order/:id" element={<DetailedOrder/>}/>
+            </Routes>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Uber Eats Restaurant Dashboard ©2022
+          </Footer>
+        </Layout>
+    </Layout>
+    
   );
 }
+
 
 export default App;
